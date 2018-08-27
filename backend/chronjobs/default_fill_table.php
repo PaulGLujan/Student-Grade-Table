22d5c57f534e4bf8c0499e7234f4f35b54a2d690
Paul Lujan <paulglujan@gmail.com>
2018-08-27 16:15:32 -0700

    Add files from default db to used db

--- backend/chronjobs/default_fill_table.php
+++ backend/chronjobs/default_fill_table.php
@@ -1,8 +1,13 @@
 <?php
 require '../mysql_connect.php';
+require 'mysql_default_db_connect.php';
+
+$query = "DELETE FROM `student_data` WHERE 1";
+$result = mysqli_query($conn, $query);
+
 
 $query = "SELECT * FROM `student_data` WHERE 1";
-$result = mysqli_query($conn, $query);
+$result = mysqli_query($conn_default_db, $query);
 
 if (empty($result)) {
 	$output['errors'][] = 'database error';
@@ -10,7 +15,13 @@ if (empty($result)) {
 	if ($result) {
         $rows=[];
         while( $row = mysqli_fetch_assoc($result)){
-            $rows[] = $row;
+            $id = $row['id'];
+            $name = $row['name'];
+            $grade = $row['grade'];
+            $course = $row['course'];
+            $update_query = "INSERT INTO `student_data`(`id`, `name`, `grade`, `course`) VALUES ($id,'{$name}',$grade,'{$course}')";
+            print($update_query);
+            mysqli_query($conn, $update_query);
         };
 	} else {
 		$output['errors'][] = 'no data';
